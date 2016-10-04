031d32b4c896cf14c342d16b4555963e75e572b4
HerrAugust <agostino.mascitti@outlook.it>
2016-10-04 19:54:12 +0200

    some adjustments for 1CreatingTasks

--- Scheduling/1CreateTask/init.c
+++ Scheduling/1CreateTask/init.c
@@ -13,26 +13,40 @@
 #include <assert.h>
 #include "commonheader.h"
 
-
 //Execution flow begins here. This is referred to as the application
 int main(void)
 {
 	rtems_status_code status[2];
-	rtems_id ids[2];
-	rtems_task_priority priorities[2] = { 2, 3 };
+	rtems_id ids[2], myid;
+	rtems_task_priority priorities[2] = { 2, 3 };//VS { 5, 6 };
 	rtems_name names[2];
 	rtems_task_priority p;
+	rtems_mode oldmode;
 
 	names[0] = rtems_build_name('C', 'T', '1', ' ');
 	names[1] = rtems_build_name('C', 'T', '2', ' ');
 
-	//information about father task (this)
-	printf("Father task has ID: %ld\n", RTEMS_SELF);
-	rtems_status_code temp = rtems_task_set_priority(RTEMS_SELF, 4, &p); //otherwise children could not be scheduled
+//Getting father's properties
+	//Getting father's ID
+	myid = rtems_task_self();
+	printf("Father task has ID: %ld\n", myid);
+
+	//Changin father's priority	
+	rtems_status_code temp = rtems_task_set_priority(myid, 4, &p); //otherwise children could not be scheduled
 	assert(temp == RTEMS_SUCCESSFUL);
-	rtems_task_set_priority(RTEMS_SELF, RTEMS_CURRENT_PRIORITY, &p); //returns current task priority
+	temp = rtems_task_set_priority(RTEMS_SELF, RTEMS_CURRENT_PRIORITY, &p); //returns current task priority
+	assert(temp == RTEMS_SUCCESSFUL);
 	printf("Father task has priority: %ld\n", p);
 
+	//Obtaining current mode
+	temp = rtems_task_mode(0, RTEMS_CURRENT_MODE, &oldmode);
+	assert(temp == RTEMS_SUCCESSFUL);
+	printf("Father task is preemptible: %d\n", oldmode & RTEMS_PREEMPT_MASK);
+
+	//Changing current mode
+
+
+//creating 2 children
 	// prototype: rtems_task_create( name, initial_priority, stack_size, initial_modes, attribute_set, *id );
 	status[0] = rtems_task_create(names[0], priorities[0], RTEMS_MINIMUM_STACK_SIZE * 2,
 		RTEMS_DEFAULT_MODES, RTEMS_DEFAULT_ATTRIBUTES, &ids[0]);
@@ -40,23 +54,30 @@ int main(void)
 		RTEMS_DEFAULT_MODES, RTEMS_DEFAULT_ATTRIBUTES, &ids[1]);
 	assert(status[0] == RTEMS_SUCCESSFUL);
 	assert(status[1] == RTEMS_SUCCESSFUL);
-	printf("2 tasks created successfully, but they are dormant\n");
+	printf("2 tasks created successfully (id: %ld and %ld), but they are dormant\n", ids[0], ids[1]);
+
 
 	// prototype: rtems_task_start(id, entry_point, argument)
 	status[0] = rtems_task_start(ids[0], task1_entrypoint, priorities[0]); //Notice: at this point, this task is pre-empted and control goes to function task1_entrypoint(), defined in task.c
+	printf("qui1\n");
 	status[1] = rtems_task_start(ids[1], task2_entrypoint, priorities[1]);
+	printf("qui2\n");
 	assert(status[0] == RTEMS_SUCCESSFUL);
 	assert(status[1] == RTEMS_SUCCESSFUL);
 	printf("2 tasks started successfully and they are ready\n");
 
-	// prototype: rtems_task_delete(id);
+
+
+	/*// prototype: rtems_task_delete(id);
 	status[0] = rtems_task_delete(ids[0]);
 	status[1] = rtems_task_delete(ids[1]);
 	assert(status[0] == RTEMS_SUCCESSFUL);
 	assert(status[1] == RTEMS_SUCCESSFUL);
-	printf("2 tasks deleted successfully\n");
+	printf("2 tasks deleted successfully\n");*/
 
-	exit(0);
+
+//busy wait
+	while(1) {}
 }
 
 /* end of file */
--- Scheduling/1CreateTask/tasks.c
+++ Scheduling/1CreateTask/tasks.c
@@ -1,12 +1,16 @@
 #include "commonheader.h"
 #include <stdio.h>
 
+rtems_id id;
+
 rtems_task task1_entrypoint(rtems_task_argument priority)
 {
-	printf("This is the task with id %ld and with priority %d \n", RTEMS_SELF, priority);
+	id = rtems_task_self();
+	printf("This is the task with id %ld and with priority %d\n", id, priority);
 }
 
 rtems_task task2_entrypoint(rtems_task_argument priority)
 {
-	printf("This is the task with id %ld and with priority %d \n", RTEMS_SELF, priority);
+	id = rtems_task_self();
+	printf("This is the task with id %ld and with priority %d\n", id, priority);
 }
